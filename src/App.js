310293769ac26b78c9f20dee58cb73b44b719664
jordung <jordanayd@gmail.com>
2023-06-21 19:51:57 +0800

    finished 2.E.3 Instagram Posts exercise

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { database, storage } from "./firebase";
 import {
   getDownloadURL,
   uploadBytes,
+  // deleteObject,
   ref as storageRef,
 } from "firebase/storage";
 import logo from "./logo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
 import "./App.css";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+const STORAGE_MESSAGES_KEY = "messages/";
 
 class App extends React.Component {
   constructor(props) {
@@ -20,6 +24,8 @@ class App extends React.Component {
     this.state = {
       messages: [],
       textInputValue: "",
+      fileInputFile: null,
+      fileInputValue: "",
     };
   }
 
@@ -45,38 +51,97 @@ class App extends React.Component {
     e.preventDefault();
     const messageListRef = databaseRef(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
-    set(newMessageRef, {
-      message: this.state.textInputValue,
-      date: new Date().toLocaleString(),
-    });
-    this.setState({
-      textInputValue: "",
-    });
+
+    const storageRefInstance = storageRef(
+      storage,
+      STORAGE_MESSAGES_KEY + this.state.fileInputFile.name
+    );
+
+    // Uploading the file online
+    uploadBytes(storageRefInstance, this.state.fileInputFile).then(
+      (snapshot) => {
+        console.log(snapshot);
+        console.log("uploaded image");
+
+        getDownloadURL(storageRefInstance).then((url) => {
+          set(newMessageRef, {
+            message: this.state.textInputValue,
+            date: new Date().toLocaleString(),
+            url: url,
+            ref: String(storageRefInstance),
+          });
+          this.setState({
+            textInputValue: "",
+            fileInputFile: null,
+            fileInputValue: "",
+          });
+        });
+      }
+    );
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
+
     let messageListItems = this.state.messages.map((message) => (
-      <li key={message.key}>
-        {message.val.date} - {message.val.message}
-      </li>
+      <Card
+        key={message.key}
+        style={{
+          width: "50vw",
+          height: "50vh",
+          margin: "1em",
+          padding: "2em",
+        }}
+      >
+        <Card.Title>{message.val.date}</Card.Title>
+        <div style={{ maxHeight: "90%" }}>
+          <img
+            style={{ height: "90%", width: "90%", objectFit: "contain" }}
+            src={message.val.url}
+            alt={message.val.name}
+          />
+          {/* <br />
+          <button
+            onClick={() => {
+              const ImageToDeleteRef = storageRef(storage, message.val.ref);
+              deleteObject(ImageToDeleteRef).then(() => console.log("deleted"));
+            }}
+          >
+            Delete
+          </button> */}
+        </div>
+        <Card.Text>{message.val.message}</Card.Text>
+      </Card>
     ));
+
+    // Newest posts will come out on top
+    messageListItems.reverse();
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form>
+          <form className="form">
             <input
               type="text"
               value={this.state.textInputValue}
               onChange={this.handleChange}
+              placeholder="Enter your message"
+            />
+            <input
+              type="file"
+              value={this.state.fileInputvalue}
+              onChange={(e) => {
+                this.setState({
+                  fileInputFile: e.target.files[0],
+                  fileInputvalue: e.target.value,
+                });
+              }}
+              placeholder="Add file here "
             />
             <button onClick={this.handleSubmit}>Send</button>
           </form>
-          <ol>{messageListItems}</ol>
+          {messageListItems}
         </header>
       </div>
     );
